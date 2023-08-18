e98f601564e7ee00f3d4e44af706f96e0c3f248c
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-08-18 18:24:43 +0300

    добавила стрелки и пагинацию для карусели

--- src/components/screens/home/header/header.tsx
+++ src/components/screens/home/header/header.tsx
@@ -1,20 +1,28 @@
 import { FC } from 'react';
-import style from './header.module.sass';
+
+import { Pagination, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import style from './header.module.sass';
+
 const Header: FC = () => {
   return (
     <div className="wrap">
       <header>
         <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}>
+          className={style.swiper}
+          pagination={{
+            type: 'fraction',
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}>
           <SwiperSlide>Slide 1</SwiperSlide>
           <SwiperSlide>Slide 2</SwiperSlide>
           <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          ...
         </Swiper>
       </header>
     </div>
