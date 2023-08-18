3035f6c8ec18a026bc7d98c920b6bb4b940e1e12
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-08-18 17:53:36 +0300

    добавила карусель

--- src/components/screens/home/header/header.tsx
+++ src/components/screens/home/header/header.tsx
@@ -1,10 +1,21 @@
 import { FC } from 'react';
 import style from './header.module.sass';
+import { Swiper, SwiperSlide } from 'swiper/react';
 const Header: FC = () => {
   return (
     <div className="wrap">
       <header>
-        <div className={style.image}></div>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}>
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          ...
+        </Swiper>
       </header>
     </div>
   );
