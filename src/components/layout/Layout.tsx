12a4b124c5c6f4c37ba9da658e64c7e2e0800331
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-08-31 11:16:27 +0300

    сверстала футер для мобильной версии

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,14 +2,15 @@ import { FC, PropsWithChildren } from 'react';
 import Meta from '../seo/Meta';
 import { IMeta } from '../seo/meta.interface';
 import Header from './header/header';
+import Footer from './footer/footer';
 
 const Layout: FC<PropsWithChildren<IMeta>> = ({ children, title, description }) => {
   return (
     <>
       <Meta title={title} description={description} />
       <Header />
-
       {children}
+      <Footer />
     </>
   );
 };
