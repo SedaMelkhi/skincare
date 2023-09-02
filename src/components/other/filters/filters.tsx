1fc1908996590c266c30cd2e2605a7c980598990
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-09-02 21:06:21 +0300

    добавила переключатель

--- src/components/other/filters/filters.tsx
+++ src/components/other/filters/filters.tsx
@@ -6,9 +6,20 @@ import style from './filters.module.sass';
 
 const Filters: FC = () => {
   return (
-    <div className={style.wrap}>
-      <span className={style.text}>фильтры</span>
-      <img src={filtersSvg.src} alt="" />
+    <div className={style.filters + ' filters'}>
+      <div className={style.wrap}>
+        <span className={style.text}>фильтры</span>
+        <img src={filtersSvg.src} alt="" />
+      </div>
+      <aside className={style.aside}>
+        <div className={style.line}>
+          <span>Cо скидкой</span>
+          <label className={style.switch}>
+            <input type="checkbox" className={style.input} />
+            <span className={`${style.slider} ${style.round}`}></span>
+          </label>
+        </div>
+      </aside>
     </div>
   );
 };
