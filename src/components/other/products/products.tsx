3ad244c8e25f6292bd12ea9fa37b07cfaca9fee8
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-09-05 12:44:42 +0300

    .

--- src/components/other/products/products.tsx
+++ src/components/other/products/products.tsx
@@ -59,6 +59,42 @@ const Products: FC = () => {
           <div className={style.new}>2 234 ₽</div>
         </div>
       </div>
+      <div className={style.cart_big}>
+        <div className={style.image}></div>
+        <div className={style.name}>SKIN&LAB Porebarrier Clear Pad очищающие пэды</div>
+        <div className={style.size}>20 мл</div>
+        <div className={style.price}>
+          <div className={style.new}>2 234 ₽</div>
+          <div className={style.old}>2 234 ₽</div>
+          <div className={style.info}>
+            <img src={infoSvg.src} alt="" />
+          </div>
+        </div>
+      </div>
+      <div className={style.cart}>
+        <div className={style.image}></div>
+        <div className={style.name}>SKIN&LAB Porebarrier Clear Pad очищающие пэды</div>
+        <div className={style.size}>20 мл</div>
+        <div className={style.price}>
+          <div className={style.new}>2 234 ₽</div>
+        </div>
+      </div>
+      <div className={style.cart}>
+        <div className={style.image}></div>
+        <div className={style.name}>SKIN&LAB Porebarrier Clear Pad очищающие пэды</div>
+        <div className={style.size}>20 мл</div>
+        <div className={style.price}>
+          <div className={style.new}>2 234 ₽</div>
+        </div>
+      </div>
+      <div className={style.cart}>
+        <div className={style.image}></div>
+        <div className={style.name}>SKIN&LAB Porebarrier Clear Pad очищающие пэды</div>
+        <div className={style.size}>20 мл</div>
+        <div className={style.price}>
+          <div className={style.new}>2 234 ₽</div>
+        </div>
+      </div>
     </div>
   );
 };
