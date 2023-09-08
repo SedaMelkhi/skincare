9144ffbb4627b0dd9c857e40db6dc6d20cd8f241
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-09-08 23:24:10 +0300

    испр-ла анимацию используя react-transition-group

--- src/components/layout/basket/basket.tsx
+++ src/components/layout/basket/basket.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import { CSSTransition } from 'react-transition-group';
+
 import style from './basket.module.sass';
 
 interface BasketProps {
@@ -9,16 +11,22 @@ interface BasketProps {
 
 const Basket: FC<BasketProps> = ({ basketOpen, setBasketOpen }) => {
   return (
-    <div
-      className={
-        style.basket__wrap +
-        ' ' +
-        (basketOpen ? style.basket__wrap_show : style.basket__wrap_hidden)
-      }>
-      <div className={style.basket}>
-        <span onClick={() => setBasketOpen(false)}>close</span>
+    <CSSTransition
+      in={basketOpen}
+      timeout={700}
+      classNames={{
+        enter: style.slideEnter,
+        enterActive: style.slideEnterActive,
+        exit: style.slideExit,
+        exitActive: style.slideExitActive,
+      }}
+      unmountOnExit>
+      <div className={style.basket__wrap}>
+        <div className={style.basket}>
+          <span onClick={() => setBasketOpen(false)}>close</span>
+        </div>
       </div>
-    </div>
+    </CSSTransition>
   );
 };
 export default Basket;
