f3d635a97ced405fc289a91d4c99096f62a49c19
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-09-13 23:19:47 +0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,5 @@
 import HomePage from '@/components/screens/home/HomePage';
-import { useEffect } from 'react';
 
 export default function Home() {
-  useEffect(() => {
-    fetch('http://fkmva.beget.tech/api/order.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        type: 'currentUser',
-      }),
-    })
-      .then((response) => response.text()) // Сначала получаем текст ответа
-      .then((text) => {
-        console.log(text); // Печатаем текст ответа для диагностики
-        return JSON.parse(text); // Затем пытаемся разобрать как JSON
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }, []);
   return <HomePage />;
 }
