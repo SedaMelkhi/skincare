72b58cdfe0a2dea6fa3ab028445680fdeeadba85
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-09-25 14:08:06 +0300

    фиксировала стикер на главной странице

--- src/components/screens/home/HomePage.tsx
+++ src/components/screens/home/HomePage.tsx
@@ -7,8 +7,6 @@ import Categories from './categories/categories';
 
 import News from './news/news';
 import Sets from './sets/sets';
-
-import style from './Home.module.sass';
 import Hits from './hits/Hits';
 import Experts from './experts/experts';
 import Journal from './jourmal/journal';
@@ -17,10 +15,15 @@ import About from './about/about';
 import Footer from '@/components/layout/footer/footer';
 import { ContactsArray } from '@/interfaces/contact.interface';
 
+import style from './Home.module.sass';
+
 const HomePage: FC<{ data: ContactsArray }> = ({ data }) => {
   const [isVisible, setIsVisible] = useState(false);
+  const [isVisibleSticker, setIsVisibleSticker] = useState(false);
   const section = useRef<HTMLDivElement | null>(null);
-
+  const sticker = useRef<HTMLDivElement | null>(null);
+  const img = useRef<HTMLImageElement | null>(null);
+  const rotate = useRef(0);
   useEffect(() => {
     const checkVisibility = () => {
       if (section.current) {
@@ -37,6 +40,23 @@ const HomePage: FC<{ data: ContactsArray }> = ({ data }) => {
           setIsVisible(false);
         }
       }
+
+      if (sticker.current) {
+        const rect = sticker.current.getBoundingClientRect();
+        // const halfHeight = sticker.current.offsetHeight;
+        // console.log(rect.top, window.innerHeight - halfHeight);
+        console.log(rect.top);
+
+        if (rect.top < 600) {
+          setIsVisibleSticker(true);
+          if (img.current) {
+            img.current.style.transform = `rotate(${rotate.current}deg)`;
+            rotate.current = -rect.top / 15;
+          }
+        } else {
+          setIsVisibleSticker(false);
+        }
+      }
     };
     window.addEventListener('scroll', checkVisibility);
     return () => {
@@ -47,12 +67,14 @@ const HomePage: FC<{ data: ContactsArray }> = ({ data }) => {
     <>
       <Layout title="Главная" description="Онлайн магазин косметики">
         <Header />
-        <main>
+        <main ref={sticker}>
           <Categories />
           <div className="wrap">
-            <Link href="/" className={style.sticker__wrap}>
-              <img src="./sticker.png" alt="" className={style.sticker} />
-            </Link>
+            {isVisibleSticker && (
+              <Link href="/" className={style.sticker__wrap}>
+                <img src="./sticker.png" alt="" className={style.sticker} ref={img} />
+              </Link>
+            )}
           </div>
           <News />
           <div ref={section} className={style.pinkMarqueeWrap}>
