f3d387ac37d4203869984d3db450ac9c65e06283
Makka <s.makka992@gmail.com>
2023-10-09 00:18:30 +0300

    добавила изменения в разделе до и после

--- src/components/screens/paidRecording/beforeAndAfter/beforeAndAfter.tsx
+++ src/components/screens/paidRecording/beforeAndAfter/beforeAndAfter.tsx
@@ -8,27 +8,29 @@ import {Pagination} from 'swiper/modules';
 const BeforeAndAfter: FC = () => {
 
     return (
-        <div className="wrap">
+        <div className="wrap berofeAndAfter">
 
             <h2 className={style.title}>ваша кожа</h2>
 
             <Swiper
                 className={style.swiper}
+                // width={568}
                 spaceBetween={20}
-                centeredSlides={true}
+                centeredSlides={false}
+                slidesPerGroup={1}
                 pagination={{
                     clickable: true,
                 }}
                 modules={[Pagination]}
                 breakpoints={{
-                    // 0: {
-                    //     slidesPerView: 0,
-                    // },
+                    0: {
+                        slidesPerView: 1.1,
+                    },
                     768: {
                         slidesPerView: 1,
                     },
                     1200: {
-                        slidesPerView: 2,
+                        slidesPerView: 1,
                     }
 
                 }}
@@ -36,29 +38,46 @@ const BeforeAndAfter: FC = () => {
 
                 <SwiperSlide className={style.slide}>
                     <div className={style.container}>
-                        <div className={style.before}><div className={style.before__img1}></div>
-                            <div className={style.before__text}>до советов агентов:(</div>
+                        <div className={style.before}>
+                            <div className={style.before__img1}></div>
+                            <div className={style.before__text}>до советов агентов:(</div>
                         </div>
-                        <div className={style.after}><div className={style.after__img1}></div>
-                            <div className={style.after__textMain}> <img src="./paidRecording/star.png" className={style.after__star}/><div className={style.after__text}>и после</div><img src="./paidRecording/star.png"  className={style.after__star}/></div>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className={style.slide}>
-                    <div className={style.container}>
-                        <div className={style.before}><div className={style.before__img2}></div>                            <div className={style.before__text}>до советов агентов:(</div>
-                        </div>
-                        <div className={style.after}><div className={style.after__img2}></div>
-                            <div className={style.after__textMain}> <img src="./paidRecording/star.png" className={style.after__star}/><div className={style.after__text}>и после</div><img src="./paidRecording/star.png"  className={style.after__star}/></div>
+                        <div className={style.after}>
+                            <div className={style.after__img1}></div>
+                            <div className={style.after__textMain}><img src="./paidRecording/star.png"
+                                                                        className={style.after__star}/>
+                                <div className={style.after__text}>и после</div>
+                                <img src="./paidRecording/star.png" className={style.after__star}/></div>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className={style.slide}>
                     <div className={style.container}>
-                        <div className={style.before}><div className={style.before__img3}></div>                            <div className={style.before__text}>до советов агентов:(</div>
+                        <div className={style.before}>
+                            <div className={style.before__img2}></div>
+                            <div className={style.before__text}>до советов агентов:(</div>
                         </div>
-                        <div className={style.after}><div className={style.after__img3}></div>
-                            <div className={style.after__textMain}> <img src="./paidRecording/star.png" className={style.after__star}/><div className={style.after__text}>и после</div><img src="./paidRecording/star.png"  className={style.after__star}/></div>
+                        <div className={style.after}>
+                            <div className={style.after__img2}></div>
+                            <div className={style.after__textMain}><img src="./paidRecording/star.png"
+                                                                        className={style.after__star}/>
+                                <div className={style.after__text}>и после</div>
+                                <img src="./paidRecording/star.png" className={style.after__star}/></div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className={style.slide}>
+                    <div className={style.container}>
+                        <div className={style.before}>
+                            <div className={style.before__img3}></div>
+                            <div className={style.before__text}>до советов агентов:(</div>
+                        </div>
+                        <div className={style.after}>
+                            <div className={style.after__img3}></div>
+                            <div className={style.after__textMain}><img src="./paidRecording/star.png"
+                                                                        className={style.after__star}/>
+                                <div className={style.after__text}>и после</div>
+                                <img src="./paidRecording/star.png" className={style.after__star}/></div>
                         </div>
                     </div>
                 </SwiperSlide>
