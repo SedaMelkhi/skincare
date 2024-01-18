a0a04eb1c40e0d6f54b8ae78650af475e06edb69
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2024-01-18 12:18:41 +0300

    .

--- src/components/screens/home/experts/experts.tsx
+++ src/components/screens/home/experts/experts.tsx
@@ -12,6 +12,7 @@ import style from './experts.module.sass';
 
 const Experts: FC<{ promoBlocks: PromoBlockArray }> = ({ promoBlocks }) => {
   const colors = [style.step_pink, style.step_violet, style.step_grey];
+  const arr = promoBlocks.filter(({ mainText }) => mainText);
   console.log(promoBlocks);
 
   return (
@@ -44,41 +45,38 @@ const Experts: FC<{ promoBlocks: PromoBlockArray }> = ({ promoBlocks }) => {
                   slidesPerView: 1.1,
                 },
               }}>
-              {promoBlocks.map(
-                ({ mainText, url, urlText }, i) =>
-                  mainText && (
-                    <SwiperSlide key={i}>
-                      <div className={style.step + ' ' + colors[i % 3]}>
-                        <div className={style.hidden}>
-                          <h3 className={style.subtitle}>{mainText}</h3>
-                          <div className={style.bottom}>
-                            <div className={style.circle}>
-                              <img src="./arrowCircle.svg" alt="" />
-                            </div>
-                            <div className={style.description}>{urlText}</div>
-                          </div>
-                          <div className={style.number}>{i > 9 ? i : '0' + (i + 1)}</div>
+              {arr.map(({ mainText, url, urlText }, i) => (
+                <SwiperSlide key={i}>
+                  <div className={style.step + ' ' + colors[i % 3]}>
+                    <div className={style.hidden}>
+                      <h3 className={style.subtitle}>{mainText}</h3>
+                      <div className={style.bottom}>
+                        <div className={style.circle}>
+                          <img src="./arrowCircle.svg" alt="" />
                         </div>
-                        <div className={style.flex}>
-                          <div className={style.left}>
-                            <div className={style.top}>
-                              <h3 className={style.title}>{urlText}</h3>
-                              <div className={style.free}>{mainText}</div>
-                            </div>
-                            <div className={style.desc}>
-                              Разбираем ваш нынешний уход, проводим разбор косметички, рассказываем,
-                              когда и что использовать, а что вовсе убрать из ухода
-                            </div>
-                            <div className={style.btn}>
-                              <Button text="Узнать подробнее" link={url} />
-                            </div>
-                          </div>
-                          <div className={style.right}></div>
+                        <div className={style.description}>{urlText}</div>
+                      </div>
+                      <div className={style.number}>{i > 9 ? i : '0' + (i + 1)}</div>
+                    </div>
+                    <div className={style.flex}>
+                      <div className={style.left}>
+                        <div className={style.top}>
+                          <h3 className={style.title}>{urlText}</h3>
+                          <div className={style.free}>{mainText}</div>
+                        </div>
+                        <div className={style.desc}>
+                          Разбираем ваш нынешний уход, проводим разбор косметички, рассказываем,
+                          когда и что использовать, а что вовсе убрать из ухода
+                        </div>
+                        <div className={style.btn}>
+                          <Button text="Узнать подробнее" link={url} />
                         </div>
                       </div>
-                    </SwiperSlide>
-                  ),
-              )}
+                      <div className={style.right}></div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
             </Swiper>
           </div>
         </div>
